c1c137f676f70d48b614847b0f4eed2aa168f7c2
pradipbhendekar <130246979+pradipbhendekar@users.noreply.github.com>
2025-05-31 23:57:38 +0530

    Time: 1 ms (33.36%) | Memory: 42.2 MB (11.00%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -5,8 +5,7 @@ class Solution {
             return 0;
         }else{
             String[] str=s.split(" ");
-             String last=str[str.length-1];
-              size=last.length();
+            size=str[str.length-1].length();
         }  
         return size;
     }
