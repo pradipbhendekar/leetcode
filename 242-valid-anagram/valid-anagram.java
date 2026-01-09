3447bc046f20ad411c407f304f07f8a03f4e14ac
pradipbhendekar <130246979+pradipbhendekar@users.noreply.github.com>
2026-01-09 15:57:09 +0530

    Time: 6 ms (42.37%) | Memory: 44.1 MB (91.48%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,25 +1,41 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    Map<Character,Integer> map=new HashMap<>();
+    // Map<Character,Integer> map=new HashMap<>();
    
-        if(s.length()!=t.length()) return false;
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-           if(!map.containsKey(ch)){
-                map.put(ch,1);
-           }else{
-            map.put(ch,map.get(ch)+1);
-           } 
+    //     if(s.length()!=t.length()) return false;
+    //     for(int i=0;i<s.length();i++){
+    //         char ch=s.charAt(i);
+    //        if(!map.containsKey(ch)){
+    //             map.put(ch,1);
+    //        }else{
+    //         map.put(ch,map.get(ch)+1);
+    //        } 
+    //     }
+
+    //     for(int i=0;i<t.length();i++){
+    //         char ch=t.charAt(i);
+    //        if(map.containsKey(ch))  map.put(ch,map.get(ch)-1);
+    //     }
+        
+    //   for(Map.Entry<Character,Integer> entry: map.entrySet()){
+    //     if(entry.getValue()>0 || entry.getValue()<0) return false;
+    //   }
+    //    return true;
+
+
+    if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
 
-        for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-           if(map.containsKey(ch))  map.put(ch,map.get(ch)-1);
+        for (int val : count) {
+            if (val != 0) return false;
         }
-        
-      for(Map.Entry<Character,Integer> entry: map.entrySet()){
-        if(entry.getValue()>0 || entry.getValue()<0) return false;
-      }
-       return true;
+
+        return true;
     }
 }
