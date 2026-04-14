84006e5c1de290e59d67402db5c19d11a7ac4937
pradipbhendekar <130246979+pradipbhendekar@users.noreply.github.com>
2026-04-14 11:37:09 +0530

    Time: 151 ms (5.04%) | Memory: 47.1 MB (5.70%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,23 +1,27 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
     
-        //now i am solve this using a sliding window and the hashtable
-        Map<Character,Integer> map=new HashMap<>();
-        int right=0;
-        int left=0;
-        int length=0;
-        while(left<s.length()){
-            char ch=s.charAt(left);
-            if(!map.containsKey(ch)){
-                map.put(ch,1);
-                left++;
-            }else{
-                map.remove(s.charAt(right));
-                right++;
-            }
-            length=Math.max(length,left-right);
-        }
+    if(s.length()== 1) return 1;
+    // this question is solve using a hashtable
+    Map<Character,Integer> map=new HashMap<>();
 
-        return length;
+    // here we are used the sliding window approch to find the lingest non-reapeating string
+    int left=0;
+    int right=0;
+    int length=0;
+    while(right<s.length()){
+       if(! map.containsKey(s.charAt(right))){
+        map.put(s.charAt(right),1);
+        right++;
+       }else{
+        length=Math.max(length,map.size());
+        map.clear();
+        left++;
+        right=left;
+       }
+       
+    length=Math.max(length,map.size());
+    }
+       return length;
     }
 }
